d520a752cb0d9a14e03ee58737435f49f03b2fea
Sujal Sharma <ftblldtzz@gmail.com>
2025-07-15 16:48:32 +0530

    Update typeWriter.js

--- js/typeWriter.js
+++ js/typeWriter.js
@@ -1,7 +1,7 @@
 
 let i = 0;
-let text1 = "Hey! Miss Qiu.";
-let text2 = "Today is the Big Day."
+let text1 = "Hey! Meri Jaan😭🎀.";
+let text2 = "Today is the Big Day...actually the best day meri bachi😭🎀"
 let speed = 100;
 
 function typeWriter(text, para){
